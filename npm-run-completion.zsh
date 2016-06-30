f09433e60c0ea3833c04702056fa8ee7c1e42ca5
Luke Childs <lukechilds123@gmail.com>
2016-06-30 22:10:09 +0100

    Only run on npm run

--- npm-run-completion.zsh
+++ npm-run-completion.zsh
@@ -1,15 +1,17 @@
 _npm_run_completion() {
-  local filename="package.json"
-  local dir=$PWD
-  while [ ! -e "$dir/$filename" ]; do
-    dir=${dir%/*}
-    if [ "$dir" = "" ]; then
-        break
-      fi
-  done
-  if [ ! "$dir" = "" ] && type node > /dev/null; then
-    local options=("${(@f)$(node -e "var pkg = require('$dir/$filename'); pkg.scripts && Object.keys(pkg.scripts).forEach(function(script) { console.log(script.replace(':', '\\\:')+':$ '+pkg.scripts[script]) })")}")
-    _describe 'values' options
+  if [ "${words[2]}" = "run" ]; then
+    local filename="package.json"
+    local dir=$PWD
+    while [ ! -e "$dir/$filename" ]; do
+      dir=${dir%/*}
+      if [ "$dir" = "" ]; then
+          break
+        fi
+    done
+    if [ ! "$dir" = "" ] && type node > /dev/null; then
+      local options=("${(@f)$(node -e "var pkg = require('$dir/$filename'); pkg.scripts && Object.keys(pkg.scripts).forEach(function(script) { console.log(script.replace(':', '\\\:')+':$ '+pkg.scripts[script]) })")}")
+      _describe 'values' options
+    fi
   fi
 }
 
